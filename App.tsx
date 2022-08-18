750acf5c6e5585267fc3528431f52175d5e15846
dimaokk <dimadark216@gmail.com>
2022-08-18 21:45:13 +0300

    fox

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-
 import Navigation from "./src/navigation/Navigation";
-import { NativeBaseProvider} from "native-base";
-=======
-import { NativeBaseProvider } from "native-base"
-import Navigation from "./src/navigation/Navigation"
->>>>>>> 5c4ca7127f357b2fb23361aedf800a5d18dbc6d3
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-<<<<<<< HEAD
       <Navigation />
-=======
-      <Navigation/>
->>>>>>> 5c4ca7127f357b2fb23361aedf800a5d18dbc6d3
     </NativeBaseProvider>
   );
 }
